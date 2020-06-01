df7a8503ef6f4c77dc3451b4d0a9f5aa9c7fc8f9
Robinkariuki <robinkaris7@gmail.com>
2020-06-01 21:48:45 +0300

    commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import {createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from './reducers/comicsReducer'
 const store = createSrore(reducer,composeWithDevTools(applyMiddleware(thunk),));
 
 
