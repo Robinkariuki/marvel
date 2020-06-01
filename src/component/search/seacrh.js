6569219f11aad98a940a04bbf742b89d99b23e29
Robinkariuki <robinkaris7@gmail.com>
2020-06-01 19:31:32 +0300

    created store

--- src/component/search/seacrh.js
+++ src/component/search/seacrh.js
@@ -3,7 +3,7 @@ import React,{Component} from 'react';
 
 class Search extends Component{
     render(){
-        return(<div><form class="form-inline d-flex justify-content-center md-form form-sm mt-0">
+        return(<div><form className="form-inline d-flex justify-content-center md-form form-sm mt-0">
         <i className="fas fa-search" aria-hidden="true"></i>
         <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search"
           aria-label="Search"></input>
