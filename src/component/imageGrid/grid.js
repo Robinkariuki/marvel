d04d676afa60eedbd36c8e13d356b45eddffb8ad
Robinkariuki <robinkaris7@gmail.com>
2020-06-03 15:22:21 +0300

    made changes

--- src/component/imageGrid/grid.js
+++ src/component/imageGrid/grid.js
@@ -27,21 +27,14 @@ class Grid extends Component{
                        <a>
                          <div className="mask rgba-white-slight"></div>
                          </a>
-
-
                            <div className="card-body">
 
-<h4 className="card-title">Card title</h4>
-<p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-          card's content.</p>
- <button type="button" className="btn btn-light-blue btn-md">Read more</button>
- 
-
-</div>
-
+                                <h4 className="card-title">Card title</h4>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                                                                    card's content.</p>
+                                <button type="button" className="btn btn-light-blue btn-md">Read more</button>
+                        </div>
                     </div>
-                    
-                
              </div>
 
       })
